76c8afe70cfe8acc5a7d0aee2c5f1708b52b5d7d
Stefan Puchmann <stefan.puchmann@gmail.com>
2024-01-17 22:16:52 +0100

    init version

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use clap::{arg, value_parser, Command};
+use std::path::PathBuf;
+
+fn main() {
+    let r_matches = cli().get_matches();
+
+    match r_matches.subcommand() {
+        Some(("cpu", matches)) => {
+            if let Some(file) = matches.get_one::<PathBuf>("file") {
+                println!("{:?}", file);
+            } else {
+                println!("no filepath found");
+            }
+        }
+        _ => unreachable!("clap should ensure we don't get here"),
+    };
+}
+
+fn cli() -> Command {
+    Command::new("pa")
+        .about("Performance aware homework")
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("cpu").about("CPU simulation").arg(
+                arg!([file] "ASM file")
+                    .required(true)
+                    .value_parser(value_parser!(PathBuf)),
+            ),
+        )
+}
